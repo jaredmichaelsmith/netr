8bdbd7513526bb81e0c587e5c8e2cd15d38f8bd0
p.golik <p.golik@1dd8cdca-7265-11de-a6a4-a7fbf15048e3>
2009-07-17 00:16:40 +0000

    gpl stuff

--- trunk/nn.py
+++ trunk/nn.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+# This file is part of netr.
+# 
+# netr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License 3 as published by
+# the Free Software Foundation.
+# 
+# netr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with netr.  If not, see <http://www.gnu.org/licenses/>.
 import sys, numpy, math, logging, os, random, ConfigParser
 import gc
 from progressbar import *
